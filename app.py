436786011238dd65729a5082ccfd7bf07f948772
Gabriel Freire <freire.gabrielrj@gmail.com>
2026-04-09 10:37:05 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ import base64
 # 1. CONFIGURAÇÃO DE PÁGINA (Deve ser o primeiro comando Streamlit)
 st.set_page_config(page_title="V3A Financeiro", layout="wide", initial_sidebar_state="collapsed")
 
+# O page_icon aceita um caminho para imagem local ou uma URL
+st.set_page_config(
+    page_title="V3A Financeiro", 
+    layout="centered",
+    page_icon="logo_v3a_icone.png" # Certifique-se de que este arquivo está na mesma pasta
+)
 
 # =================================================================
 # PAINEL DE LOGIN
